73bc2f79dfe5f2482ff1450dc4a2e960399077e3
KelseyKrehbiel <krehbielk@gmail.com>
2020-10-24 00:25:53 -0700

    outline functions.

--- app.js
+++ app.js
@@ -75,14 +75,93 @@ const store = {
  * 
  */
 
-/********** TEMPLATE GENERATION FUNCTIONS **********/
+/********** TEMPLATE GEN/ERATION FUNCTIONS **********/
 
 // These functions return HTML templates
+function generateQuizQuestion(questionNumber){
+  //use quesiton data to generate HTML
+  let quesiton = store[questionNumber].question;
+  let answerList = store[questionNumber].answers;
+  //start the form
+  let questionHTML = `<form action="/action_page.php">
+                        <p>{question}</p>`
+  //add radio button element for each option
+  answerList.forEach(element => {
+    questionListHTML =`<input type="radio" id={element} name="choice" value={element}>
+                      <label for={element}>{element}</label>`;
+    questionHTML += questionListHTML; 
+  });
+  //complete the form
+  questionHTML += `</form>`
+  //add questionHTML to the page
+
+}
+
+function generateAnswerComment(isCorrect){
+  //if answer correct say "correct"
+  //if answer wrong say "incorrect" and display the correct answer
+  let comment = ""
+  if(isCorrect){
+    comment = "<p>Correct</p>"
+  }
+  else{
+    comment = "Incorrect. The correct answer is {store[questionNumber].correctAnswer}"
+  }
+  //add comment to the page
+}
 
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
+function renderQuiz(){
+  //render the page with appropriate elements
+}
 
 /********** EVENT HANDLER FUNCTIONS **********/
 
 // These functions handle events (submit, click, etc)
+function handleWelcomePage(){
+  //load the welcome page
+  let welcomeHTML = `<div class="start-screen">
+                      <p>This quiz has questions about the game Dungeons and Dragons</p>
+                      <button type="button" id="start">Begin Quiz</button>
+                    </div>
+                    `;
+  return welcomeHTML;
+}
+
+function handleBeginQuiz(){
+  //when Start Quiz button is pressed show the first question
+  $('body').on('click', '#start', function (event) {
+    store.quizStarted = true;
+    renderQuiz();
+  });
+}
+
+function handleAnswerSubmitted(){
+  //get the choice selection
+  //let choice = 
+  //send to grader function
+
+  //call answer tooltip function to display correct or incorrect information
+  //
+
+}
+
+function scoreQuestion(choice){
+  //compare it to the correct answer
+  //determine if answer is correct
+  //Update quiz score
+  //return true or false if answer is correct
+  if(choice === store[questionNumber].correctAnswer){
+    score++;
+    generateAnswerComment(true);
+  }
+  else{
+    generateAnswerComment(false);
+  }
+}
+
+function handleQuizApp(){
+  renderQuiz();
+}
